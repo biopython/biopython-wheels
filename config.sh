8c1ceec53a95312b1b9e5434847c2166d658edde
Peter Cock <p.j.a.cock@googlemail.com>
2017-07-11 16:20:08 +0100

    Confirm can import compiled C code

--- config.sh
+++ config.sh
@@ -11,5 +11,8 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c 'import Bio; print(Bio.__version__)'
+    # This will confirm some of our C code compiled fine:
+    python -c "from Bio import Cluster; print(Cluster.version())"
+    python -c "from Bio.Nexus import cnexus; from Bio import cpairwise2"
     # TODO - Run our test suite...
 }
