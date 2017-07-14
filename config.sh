48df6e7c0530eeb99694a66f6afd8360206905d2
Peter Cock <p.j.a.cock@googlemail.com>
2017-07-14 14:50:06 +0100

    TravisCI: Try using update-locale

--- config.sh
+++ config.sh
@@ -8,31 +8,19 @@ function pre_build {
 }
 
 function run_tests {
-    # To help with debug any unicode issues
-    echo "Before trying to change locale..."
-    locale
-    python -c "import sys; print(sys.version); print('Default encoding: ' + sys.getdefaultencoding())"
-    python -c "import locale; print('Locale prefered encoding: ' + locale.getpreferredencoding())"
-    echo "Switching to LANG=en_US.utf8 etc"
-    # Seems there is no /etc/default/locale file...
-    sudo echo "LANG=en_US.UTF-8" > /etc/default/locale
-    sudo locale-gen en_US.UTF-8
-    sudo dpkg-reconfigure locales
-    export LANG="en_US.UTF-8"
+    echo "Forcing en_US.UTF-8 as workaround for encoding issues in Biopython 1.70 tests"
+    sudo update-locale LANG=en_US.UTF-8
     locale
     python -c "import sys; print(sys.version); print('Default encoding: ' + sys.getdefaultencoding())"
     python -c "import locale; print('Locale prefered encoding: ' + locale.getpreferredencoding())"
     # Runs tests on installed distribution from an empty directory
     python --version
-    # Debugging check for potential openssl issue in this Python
-    python -c "import hashlib; print(sorted(x for x in dir(hashlib) if not x.startswith('_')))"
     # Check the simplest import, and version for consistency
     python -c "import Bio; print('Biopython version ' + Bio.__version__)"
     # This will confirm some of our C code compiled fine:
     python -c "from Bio.Nexus import cnexus; from Bio import cpairwise2"
     python -c "from Bio import Cluster; print('Bio.Cluster version ' + Cluster.version())"
     # So far so good, now let's run our full test suite...
-    pwd
     if [ -n "$IS_OSX" ]; then
         cd ${TRAVIS_BUILD_DIR}/biopython/Tests
     else
@@ -44,5 +32,4 @@ function run_tests {
     else
 	cd /io/
     fi
-    pwd
 }
